7b8bcd2617256f7a112dfc93bbb7098f89130a4f
KristineValladolid <144756509+KristineValladolid@users.noreply.github.com>
2023-10-22 22:01:24 +0800

    Add files via upload

---
+++ laboratory3/laboratory3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                int num = ReadNumber(1, 100);
+                Console.WriteLine("Square root of the number is: " + Math.Sqrt(num));
+            }
+            catch (CustomException cx)
+            {
+                Console.WriteLine("Custom Exception: " + cx.Message);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("General Exception: " + x.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+
+    static int ReadNumber(int start, int end)
+    {
+        int num;
+        Console.Write($"Enter an integer between {start} and {end}: ");
+        if (int.TryParse(Console.ReadLine(), out num))
+        {
+            if (num >= start && num <= end)
+            {
+                return num;
+            }
+            else
+            {
+                throw new CustomException("Invalid number");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
